f37123dd84ae001a4ee32d92cf1d315299733284
Daniel Winkler <dw@dwinkler.org>
2025-09-23 12:12:27 +1000

    Added github workflow for build + deploy of the website

---
+++ flake.nix
@@ -0,0 +1,58 @@
+# filepath: flake.nix
+{
+  description = "Dev env for MA";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://rstats-on-nix.cachix.org"
+      "https://rde.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "rstats-on-nix.cachix.org-1:vdiiVgocg6WeJrODIqdprZRUrhi1JzhBnXv7aWI6+F0="
+      "rde.cachix.org-1:yRxQYM+69N/dVER6HNWRjsjytZnJVXLS/+t/LI9d1D4="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:rstats-on-nix/nixpkgs/2025-09-01";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    allSystems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (
+        system:
+          f {
+            # Use legacyPackages instead of importing the flake path manually
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+      );
+  in {
+    packages = forAllSystems ({pkgs}: {
+      default = pkgs.rWrapper.override {
+        packages =
+          # External R packages list (expects r-packages.nix to return a list)
+          import ./r-packages.nix {inherit pkgs;};
+      };
+    });
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.xquartz
+          (pkgs.rWrapper.override {
+            packages =
+              # External R packages list (expects r-packages.nix to return a list)
+              import ./r-packages.nix {inherit pkgs;};
+          })
+        ];
+      };
+    });
+  };
+}
